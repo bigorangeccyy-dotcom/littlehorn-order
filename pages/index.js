0941ef35a6df23d41d218fe469d98a653e144426
bigorangeccyy-dotcom <bigorangeccyy@gmail.com>
2025-08-07 01:03:12 +0100

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,1 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>LittleHorn 点单系统</h1>
-      <p>欢迎使用！部署成功 🎉</p>
-    </div>
-  );
-}
+export default function Home() { return <h1>LittleHorn 点单系统 - 测试成功</h1>; }
